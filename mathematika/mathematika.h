a60b7b261db5850678396d64c00732b6a08a6f51
shmahma <souhailhmahma@gmail.com>
2022-09-23 17:54:16 +0200

    first

---
+++ mathematika/mathematika.h
@@ -0,0 +1,8 @@
+#ifndef _MATHEMATICS_
+#define _MATHEMATICS_
+#include <stdbool.h>
+int mathematika_gcd(int x, int y);
+bool mathematika_isPrime(int n);
+bool mathematika_isCoPrime(int x, int y);
+#endif
+
